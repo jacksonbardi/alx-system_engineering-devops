57115e13198dade6186f245a9d8f86e70fa97b59
jacksonbardi <rachadwafir8@gmail.com>
2024-05-16 14:36:12 +0000

    Update

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-hange the OS configuration so that it is possible to login with the
+# Change the OS configuration so that it is possible to login with the
 # holberton user and open a file without any error message.
 
 exec {'OS security config':
